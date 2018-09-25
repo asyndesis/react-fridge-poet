3beb2ee49ec7e561d2361e4e5a60c53e6c5e4f49
Trevor Bonomi <trevorbonomi@gmail.com>
2018-09-25 00:02:27 -0400

    fix

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,15 @@
 const port = process.env.PORT || 8000;
+const path = require('path');
 var express = require('express');
 var app = express();
 server = app.listen(port);
 var socket = require('socket.io');
 var magnets = require('./magnets');
 io = socket(server);
+
+app.use(express.static(path.join(__dirname,'../../build')));
+app.get('/',(req,res,next) =>
+    res.sendFile(__dirname+'./index.html'));
 
 const spawnRooms = () => {
     let rooms = [
