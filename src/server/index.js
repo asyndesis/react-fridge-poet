eebd97c0e15513544d681a55242f24dc822a4ba1
Trevor Bonomi <trevorbonomi@gmail.com>
2018-09-25 01:15:39 -0400

    stuff

--- src/server/index.js
+++ src/server/index.js
@@ -8,8 +8,9 @@ const magnets = require('./magnets');
 io = socket(server);
 
 app.use(express.static(path.join(__dirname,'../../build')));
-app.get('*',(req,res,next) =>
-    res.sendFile(__dirname+'./index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../build/index.html'));
+});
 
 console.log('index.js running');
 
