17b37cfa5509547a3127e733f469367cb49b0e7a
Trevor Bonomi <trevorbonomi@gmail.com>
2018-10-06 19:12:39 -0400

    stuff

--- src/Socket.js
+++ src/Socket.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 var server = 'http://jempy.local:8000';
 //var server = 'http://Blumpkin.local:8000';
 if (process.env.NODE_ENV === 'production') { 
-    server = 'https://fridge-poet.herokuapp.com/';
+    server = 'https://fridge-poet.herokuapp.com';
 }
 export const Socket = io.connect(server);
 export default '';
