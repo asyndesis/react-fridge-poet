94af75e45894bafb71f9a60cbd9a7e19c19c1f3b
asyndesis <trevorbonomi@gmail.com>
2024-06-07 21:26:46 +0200

    fix

--- src/server/index.js
+++ src/server/index.js
@@ -8,8 +8,13 @@ const magnets = require("./magnets");
 io = socket(server);
 
 app.use(express.static(path.join(__dirname, "../../build")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../build/index.html"));
+});
+
+app.get("/health", (_, res) => {
+  res.status(200).json({ status: "ok" });
 });
 
 console.log("index.js running");
