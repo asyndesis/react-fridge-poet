a2d87f74ad6dae704d15a30ec5ce9c7c0a9992bd
asyndesis <trevorbonomi@gmail.com>
2024-06-09 22:01:27 +0200

    poormans waiting

--- src/Socket.js
+++ src/Socket.js
@@ -1,37 +1,11 @@
 import io from "socket.io-client";
 export const Socket = io.connect(process.env.REACT_APP_SOCKET_URL);
 
-let hasErrorOccurred = false;
-
-const handleSocketError = () => {
-  if (!hasErrorOccurred) {
-    hasErrorOccurred = true;
-    console.error("Socket error occurred");
-    setTimeout(() => {
-      hasErrorOccurred = false;
-      Socket.connect(); // Attempt to reconnect after a delay
-    }, 5000);
-  }
-};
-
-const handleSocketSuccess = () => {
-  if (!hasErrorOccurred) {
-    window.dispatchEvent(new Event("socket_found"));
-  }
-};
-
-Socket.on("connect", handleSocketSuccess);
-Socket.on("connect_error", handleSocketError);
-Socket.on("connect_timeout", handleSocketError);
-Socket.on("error", handleSocketError);
-Socket.on("reconnect_error", handleSocketError);
-Socket.on("reconnect_failed", handleSocketError);
-
 const interval = window.setInterval(() => {
-  console.log(process.env.REACT_APP_SOCKET_URL);
-  if (Socket.connected && !hasErrorOccurred) {
+  Socket.connect(); // Attempt to reconnect after a delay
+  if (Socket.connected) {
     window.clearInterval(interval);
   }
-}, 1000);
+}, 15000);
 
 export default Socket;
