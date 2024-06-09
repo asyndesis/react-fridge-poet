0e73bcf7b2a805478f8cd6e5ea2f57cff854e1c8
asyndesis <trevorbonomi@gmail.com>
2024-06-09 22:02:27 +0200

    poormans

--- src/Socket.js
+++ src/Socket.js
@@ -5,6 +5,7 @@ const interval = window.setInterval(() => {
   Socket.connect(); // Attempt to reconnect after a delay
   if (Socket.connected) {
     window.clearInterval(interval);
+    window.dispatchEvent(new Event("socket_found"));
   }
 }, 15000);
 
