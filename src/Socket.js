67620329247dcc3a033b849bc763ece875da9872
asyndesis <trevorbonomi@gmail.com>
2024-06-09 14:50:42 +0200

    fix error

--- src/Socket.js
+++ src/Socket.js
@@ -1,5 +1,21 @@
 import io from "socket.io-client";
 export const Socket = io.connect(process.env.REACT_APP_SOCKET_URL);
+
+let hasErrorOccurred = false;
+
+const handleSocketError = () => {
+  if (!hasErrorOccurred) {
+    hasErrorOccurred = true;
+    window.location.reload();
+  }
+};
+
+Socket.on("connect_error", handleSocketError);
+Socket.on("connect_timeout", handleSocketError);
+Socket.on("error", handleSocketError);
+Socket.on("reconnect_error", handleSocketError);
+Socket.on("reconnect_failed", handleSocketError);
+
 const interval = window.setInterval(() => {
   console.log(process.env.REACT_APP_SOCKET_URL);
   if (Socket.connected) {
@@ -7,4 +23,5 @@ const interval = window.setInterval(() => {
     window.clearInterval(interval);
   }
 }, 1000);
+
 export default Socket;
